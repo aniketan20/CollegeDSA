655f8397708aa449a5ecedcc25cfdb43a420df3b
aniketan20 <aniketsullhan456@gmail.com>
2024-02-27 12:21:54 +0530

    2/27/24

---
+++ PrimeNumber/SegmentedSeiveOfEratostenes.java
@@ -0,0 +1,53 @@
+/*
+ * time complexity :
+ * root(end-start)*log(root(end-start+1))
+ * 
+ */
+
+import java.util.*;
+
+public class SegmentedSeiveOfEratostenes {
+
+    public static void segmentedSeive(boolean visited[], List li, int len,int start){
+        for(int i = 0 ; i < li.size() ; i++){
+            for(int j = 0 ; j < len ; j++){
+                if(visited[j] == false) continue;
+                if((j+start) == 1)  visited[j] = false;
+                if((j+start)%((int)li.get(i)) == 0 && (j+start) != ((int)li.get(i))){
+                    visited[j] = false;
+                }
+            }
+        }
+        for(int i = 0 ; i < len ; i++){
+            if(visited[i]) System.out.print(i+start+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int start = 0;
+        int end = 100;
+        int len = end-start+1;
+
+        boolean visited[] = new boolean[len];
+        Arrays.fill(visited,true);
+
+        int root = (int) Math.sqrt(end);
+        boolean prime[] = new boolean[root+1];
+        Arrays.fill(prime, true);
+
+        ArrayList<Integer> li = new ArrayList<>();
+        prime[0] = false;
+        prime[1] = false;
+        for(int i = 2 ; i < root + 1 ; i++){
+            if(prime[i]){
+                for(int j = i*i ; j < root + 1 ; j+=i){
+                    prime[j] = false;
+                }
+                li.add(i);
+            }
+        }
+        segmentedSeive(visited, li, len,start);
+    }
+}
+
+
