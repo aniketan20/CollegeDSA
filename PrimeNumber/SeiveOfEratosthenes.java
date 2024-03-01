bcd5685f28ab801e9d5f54198c0a86155649177f
aniketan20 <aniketsullhan456@gmail.com>
2024-03-01 10:02:38 +0530

    updated

--- PrimeNumber/SeiveOfEratosthenes.java
+++ PrimeNumber/SeiveOfEratosthenes.java
@@ -2,21 +2,19 @@
  * n/2+n/3+n/5+n/7+n/11+..... = 
  */
 
-import java.util.*;
 
 public class SeiveOfEratosthenes {
     public static void main(String[] args) {
-        boolean[] visited = new boolean[101];
-        Arrays.fill(visited, true);
-        visited[0] = false;
-        visited[1] = false;
-        for(int i = 2 ; i < 101 ; i++){
-            if(visited[i]){
-                System.out.print(i+" ");
-                for(int j = i*i ; j < 101 ; j+=i){
-                    visited[j] = false;
+        int n = 50;
+        boolean[] visited = new boolean[n+1];
+        for(int i = 2 ; i*i <= n ; i++){
+            if(!visited[i]){
+                // System.out.print(i+" ");
+                for(int j = i*i ; j <= n ; j+=i){
+                    visited[j] = true;
                 }
             }
         }
+        for(int i = 2 ; i <= n ; i++)   if(!visited[i])  System.out.print(i+" ");
     }
 }
