7a30013a3dfdf1613a41f9c54ce53a000b9993f0
aniketan20 <aniketsullhan456@gmail.com>
2024-03-14 10:04:08 +0530

    print subset

--- BackTracking/PrintSubSet.java
+++ BackTracking/PrintSubSet.java
@@ -1,11 +1,25 @@
+/*
+ * O(2^n * n)
+ */
+
 public class PrintSubSet {
 
-    public static void printSubSet(int arr[],int vis[],int pos,String ans){
-        
+    public static void printSubSet(int arr[], StringBuilder ans ,int i){
+        if(i == arr.length){
+            System.out.println(ans);
+            return;
+        }
+        printSubSet(arr, ans, i+1);
+        ans.append(""+arr[i]);
+        printSubSet(arr, ans, i+1);
+        ans.delete(ans.length()-1,ans.length()); //pop the last index
+        return;
     }
 
     public static void main(String[] args) {
         int arr[] = {1,2,3};
-        
+        printSubSet(arr, new StringBuilder() , 0);
     }
 }
+
+
