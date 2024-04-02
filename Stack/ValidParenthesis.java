485483415cc6206a91f8b54516799ad5e4d0dd55
aniketan20 <aniketsullhan456@gmail.com>
2024-04-02 09:43:43 +0530

    valid parenthisis

---
+++ Stack/ValidParenthesis.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        HashMap<Character,Character> table = new HashMap<>();
+        table.put(')','(');
+        table.put(']','[');
+        table.put('}','{');
+        
+        Stack<Character> stack = new Stack<>();
+        String str = "{}()";
+
+
+        for(char ch : str.toCharArray()){
+            if(table.containsKey(ch)){
+                if(!stack.isEmpty() && table.get(ch) == stack.peek()){
+                    stack.pop();
+                }
+                else{
+                    System.out.println(false);
+                    return;
+                }
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        
+        System.out.println(stack.isEmpty());
+    }
+}
