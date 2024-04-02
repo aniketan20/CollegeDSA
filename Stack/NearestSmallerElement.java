ecf92bbbeda6d300552ca4791155d9ca30ecf67b
aniketan20 <aniketsullhan456@gmail.com>
2024-04-02 10:30:40 +0530

    Nearest Smaller Element

---
+++ Stack/NearestSmallerElement.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class NearestSmallerElement {
+    public static void main(String[] args) {
+        int arr[] = new int[]{7,3,8,4,-4,13,7,14};
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0 ; i < n ; i++) {
+            while(!stack.isEmpty() && stack.peek() >= arr[i]) {
+                stack.pop();
+            }
+            if(stack.isEmpty()) {
+                list.add(-1);
+            }
+            else {
+                list.add(stack.peek());
+            }
+            stack.push(arr[i]);
+        }
+
+        System.out.println(list);
+    }
+}
