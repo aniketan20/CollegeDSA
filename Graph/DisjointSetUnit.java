19d9733e73cb65f4ee8b16bf79f3e535a22a8748
aniketan20 <aniketsullhan456@gmail.com>
2024-04-30 10:56:21 +0530

    more on graph

---
+++ Graph/DisjointSetUnit.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DisjointSetUnit {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void DSU(HashMap<Integer,ArrayList<Integer>> hm , int[] p){
+        // for(int x : p) System.out.print(x+" ");
+        //     System.out.println();
+        int t = sc.nextInt();
+        while(t > 0){
+
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int su = p[u];
+            int sv = p[v];
+
+            int ru = 0;
+            int rv = 0;
+
+            while(ru != su){
+                ru = su;
+                su = p[su];
+            }
+
+            while(rv != sv){
+                rv = sv;
+                sv = p[sv];
+            }
+
+            if(rv == ru){
+                System.out.println("false");
+            }
+            else{
+                System.out.println("true");
+                hm.get(Math.max(u,v)).add(Math.min(u,v));
+                p[u] = Math.min(ru,rv);
+                p[v] = Math.min(ru,rv);
+            }
+            // for(int x : p) System.out.print(x+" ");
+            // System.out.println();
+            t--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+        
+        int n = sc.nextInt();
+        int parent[] = new int[n+1];
+        parent[0] = -1;
+
+        for (int i = 1; i <= n; i++) {
+            hm.put(i, new ArrayList<>());
+            parent[i] = i;
+        }
+        int e = sc.nextInt();
+
+        for (int i = 0; i < e; i++) {
+            int start = sc.nextInt();
+            int dest = sc.nextInt();
+
+            hm.get(start).add(dest);
+            parent[start] = dest;
+        }
+
+        DSU(hm,parent);
+    }
+}
+/*
+6
+3
+2 1
+3 2
+5 4
+10
+ */
--- Graph/TopologicalSort.java
+++ Graph/TopologicalSort.java
@@ -1,11 +1,35 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class TopologicalSort {
 
-    public static void topologicalSort(HashMap<Integer, ArrayList<Integer>> hm) {
-        
+    public static void topologicalSort(HashMap<Integer, ArrayList<Integer>> hm, int indegree[]) {
+        ArrayList<Integer> visited = new ArrayList<Integer>();
+        ArrayList<Integer> sortedOrder = new ArrayList<Integer>();
+
+        Queue<Integer> qu = new LinkedList<>();
+
+        for(int i = 1 ; i < indegree.length ; i++){
+            if(indegree[i] == 0){
+                qu.add(i);
+                visited.add(i);
+            }
+        }
+
+        while(!qu.isEmpty()){
+            int x = qu.poll();
+            visited.add(x);
+            System.out.print(x+" ");
+            for(int i : hm.get(x)){
+                if(visited.contains(i)){
+                    System.out.println("Cycle found");
+                    return;
+                }
+                indegree[i]--;
+                if(indegree[i] == 0){
+                    qu.add(i);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -13,8 +37,11 @@ public class TopologicalSort {
         Scanner sc = new Scanner(System.in);
 
         HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
-
+        
         int n = sc.nextInt();
+        
+        int indegree[] = new int[n+1];
+
         for (int i = 1; i <= n; i++) {
             hm.put(i, new ArrayList<>());
         }
@@ -25,7 +52,8 @@ public class TopologicalSort {
             int dest = sc.nextInt();
 
             hm.get(start).add(dest);
+            indegree[dest]++;
         }
-        topologicalSort(hm);
+        topologicalSort(hm,indegree);
     }
 }
